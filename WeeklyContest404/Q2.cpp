1067688f83ffe52bf36a9dcfe1d48ebe8c81927a
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-06-30 09:32:13 +0530

    Update Q2.cpp

--- WeeklyContest404/Q2.cpp
+++ WeeklyContest404/Q2.cpp
@@ -1,1 +1,44 @@
 
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int n = nums.size();
+        int cnt1 = 0, cnt2 = 0;
+        for(int i=0; i<n; i++) {
+        cnt1 += (nums[i]%2); cnt2 += (nums[i]%2==0);}
+        int res = max(cnt1, cnt2);
+        // odd even odd ...
+        // even odd even...
+        
+        int ok = 0;
+        cnt1 = 0, cnt2 = 0;
+        for(int i=0; i<n; i++) {
+            if(!ok) {
+                if(nums[i]%2){
+                    cnt1++;
+                    ok^=1;
+                }
+            } else {
+                if(nums[i]%2==0) {
+                    cnt1++;
+                    ok^=1;
+                }
+            }
+        }
+        ok=0;
+        for(int i=0; i<n; i++) {
+            if(!ok) {
+                if(nums[i]%2==0){
+                    cnt2++;
+                    ok^=1;
+                }
+            } else {
+                if(nums[i]%2) {
+                    cnt2++;
+                    ok^=1;
+                }
+            }
+        }
+        return max({res, cnt1, cnt2});
+    }
+};
