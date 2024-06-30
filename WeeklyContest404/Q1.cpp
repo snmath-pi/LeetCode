9c53cb8a225719f41e117e5fccbcd16fad4c9026
Saksham Negi <95710218+snmath-pi@users.noreply.github.com>
2024-06-30 09:32:15 +0530

    Update Q1.cpp

--- WeeklyContest404/Q1.cpp
+++ WeeklyContest404/Q1.cpp
@@ -1,1 +1,51 @@
 
+class Solution {
+public:
+    int maxHeightOfTriangle(int red, int blue) {
+        int res1 = 0, res2 = 0;
+        int ok = 1;
+        int cur1 = 1, cur2 = 2;
+        int ored = red,oblue=blue;
+        while(true) {
+            if(ok) {
+                if(red>=cur1) {
+                    red-=cur1;
+                    cur1+=2;
+                    
+                    res1++;
+                } else break;
+            } else {
+                if(blue>=cur2) {
+                    blue-=cur2;
+                    cur2+=2;
+                    
+                    res1++;
+                } else break;
+            }
+            ok^=1;
+        }
+        cur1=2,cur2=1;
+        ok=0;
+        red=ored,blue=oblue;
+        while(true) {
+            if(ok) {
+                if(red>=cur1) {
+                                        red-=cur1;
+
+                    cur1+=2;
+                    res2++;
+                } else break;
+            } else {
+                if(blue>=cur2) {
+                                        blue-=cur2;
+
+                    cur2+=2;
+                    res2++;
+                } else break;
+            }
+            ok^=1;
+        }
+        return max(res1, res2);
+
+    }
+};
